b212ad6715f1f26bf2c7ff0a3573a6e18b363c67
IamEld3st <iameld3st@gmail.com>
2022-08-10 09:01:07 +0200

    Pseudo code description

--- kafka_python_consumer/consumer.py
+++ kafka_python_consumer/consumer.py
@@ -6,7 +6,30 @@ consumer = KafkaConsumer('rover-zonechange',
                          bootstrap_servers='rover-cluster-kafka-bootstrap:9092',
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
+route = [
+    moon_outpost_a,
+    moon_checkpoint_a,
+    moon_outpost_b,
+    moon_checkpoint_b,
+    ...
+]
+
 last_seen = {}
+
+current_travel_time = {
+    "moon_outpost_a->moon_checkpoint_a": {
+        duration: 50.2,
+    },
+    {...}
+}
+
+predicted_times = {
+    "rover_id":{
+        moon_outpost_a: timestamp,
+        moon_checkpoint_a: timestamp,
+        ...
+    }
+}
 
 for msg in consumer:
     data = msg.value
@@ -22,6 +45,9 @@ for msg in consumer:
         print(f"Rover {data['carId']} entered {data['nextZoneId']} at {time.time()}")
         if data['carId'] in last_seen:
             print(f"  - This rover was last seen in {last_seen[data['carId']]['last_zone']} trip took {time.time()-last_seen[data['carId']]['timestamp']}")
+            # Calculate travel time between points
+            # Update predicted times using new durations
+            #   - using last_seen_at timestamp + the travel time should create info about when is the aproximate arrival time
         
     else:
         print('Zone update malformed.')
